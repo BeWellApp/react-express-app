2a7cbb09a47f24b7e0e980d3b914a8fca5c02e07
Daniel M <95mangisto.gmail.com>
2020-11-30 19:14:01 +0200

    nextButton

--- client/components/NextButton.js
+++ client/components/NextButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity, Button, Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function NextButton() {
+export default function NextButton({ onPress }) {
     return (
-        <View>
-            <Text>NextButton</Text>
-        </View>
+        <TouchableOpacity title="Next" onPress={onPress} >
+        <MaterialIcons name='navigate-before' size={30} />
+        </TouchableOpacity>
+
     )
 }
--- client/screens/SelectLan.js
+++ client/screens/SelectLan.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import BackButton from '../components/BackButton';
+import NextButton from '../components/NextButton';
 
-export default function SelectLan() {
+export default function SelectLan({ navigation }) {
     return (
         <View>
             <Text>SelectLan</Text>
+            <View style={{
+                display: "flex",
+                flexDirection: "row"
+            }}>
+                <View style={{
+                    flex: 1
+                }}>
+            <NextButton onPress={() => {navigation.navigate("Welcome") }} />
+            </View><View style={{
+                flex: 1
+            }}>
+            <BackButton onPress={() => {navigation.navigate("Splash") }} size={0.1}/>
+            </View>
+            </View>
         </View>
     )
 }
