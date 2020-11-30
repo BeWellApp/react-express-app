c5f5d24ba98d6082522c4c652bb9d1a20c499165
yakov avraham <yakov.avra@gmail.com>
2020-11-30 12:44:12 +0200

    add screens and components files

---
+++ client/components/PlayButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function PlayButton() {
+    return (
+        <View>
+            <Text>PlayButton</Text>
+        </View>
+    )
+}
