d6009038da3bc21b0fa3f1af1a0f642ac6508857
Hackathon2020-BeWell <74921795+Hackathon2020-BeWell@users.noreply.github.com>
2020-12-02 16:04:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api/content', contentRouter);
 app.use('/api/users', usersRouter);
 
 //*Connect to mongodb
-const mongoUrl = process.env.SB_URL || "mongodb://localhost:27017/beWell";
+const mongoUrl = process.env.SB_URL || "mongodb+srv://Haktoon2020:BeWell2020@bewelldb.6z3sg.mongodb.net/BeWellDB?retryWrites=true&w=majority";
 
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true, useUnifiedTopology: true
