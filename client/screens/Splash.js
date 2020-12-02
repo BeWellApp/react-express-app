021ebcd2c602a079d9942a2e58d3911485e8c3a4
yakov avraham <yakov.avra@gmail.com>
2020-12-02 12:46:16 +0200

    navigate to MainMenu unless user is not found

--- client/screens/Splash.js
+++ client/screens/Splash.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { View, Text,ImageBackground,Image,StyleSheet  } from 'react-native';
+import React, { useEffect } from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-
+import Constants from 'expo-constants';
+import {getUser} from '../apis/server/user';
 const styles = StyleSheet.create({
     logoContainer: {
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#f7f7f7'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f7f7f7'
     },
     logo: {
-      width:'100%',
-      height:'100%',
+        width: '100%',
+        height: '100%',
     }
-  });
-export default function Splash({navigation}) {
+});
+export default function Splash({ navigation }) {
+    let nextPage = 'MainMenu';
     setTimeout(() => {
-        navigation.navigate('SelectLan')
+        navigation.navigate(nextPage)
     }, 2000);
+
+    useEffect(() => {
+        (async () => {
+            const user = await getUser(Constants.installationId);
+            if(user === "user not found") {
+                nextPage = 'SelectLan';
+            }
+        })();
+    }, []);
+
     return (
         <SafeAreaView style={styles.logoContainer}>
-        <ImageBackground resizeMode="contain" style={styles.logo} source={require('../assets/Logo.jpeg')}/>  
-
+            <ImageBackground resizeMode="contain" style={styles.logo} source={require('../assets/Logo.jpeg')} />
         </SafeAreaView>
 
-        
+
     );
 }
